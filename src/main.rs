140bd2adc4bdb6e9347acc7842e946b27616933c
Max Block <maxblock@pm.me>
2020-09-21 13:56:00 +0300

    update

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use mongodb::{bson::doc, error::Error, options::FindOptions, sync::Client};
+
+fn main() -> Result<(), Error> {
+    let client = Client::with_uri_str("mongodb://localhost")?;
+    let col = client.database("test").collection("test");
+    col.drop(None)?;
+
+    col.insert_many(vec![doc! {"name": "n1"}, doc! {"name": "n2"}], None)?;
+
+    let find_options = FindOptions::builder().sort(doc! { "name": -1}).build();
+    let res: Vec<_> = col.find(doc! {}, find_options).unwrap().collect();
+
+    println!("{:#?}", res);
+
+    Ok(())
+}
