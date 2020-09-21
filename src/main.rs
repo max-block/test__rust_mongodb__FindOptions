67610129b224888c3f282943ab1083df7179e899
Max Block <maxblock@pm.me>
2020-09-21 14:20:43 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() -> Result<(), Error> {
     col.insert_many(vec![doc! {"name": "n1"}, doc! {"name": "n2"}], None)?;
 
     // FindOptions::builder <-- intellij-rust and vscode can't autocomplete it
+
     let find_options = FindOptions::builder().sort(doc! { "name": -1}).build();
     let res: Vec<_> = col.find(doc! {}, find_options).unwrap().collect();
 
